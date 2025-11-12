5a2037a2073770b7e40bb52e526576c3f1137681
Jorge Fabian Martinez Sanchez <194762872+Sickboooooy@users.noreply.github.com>
2025-11-12 02:41:05 -0600

    Create white-light tree variants for immersive experience

---
+++ client/src/pages/immersive-experience.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import ImmersiveExperience from "@/components/immersive-experience";
+
+const ImmersiveExperiencePage: FC = () => {
+  return <ImmersiveExperience />;
+};
+
+export default ImmersiveExperiencePage;
